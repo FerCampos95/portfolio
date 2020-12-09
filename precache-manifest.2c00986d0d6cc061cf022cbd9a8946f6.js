45e015adf0b0f9bd88365b5d4f6528b516ec3fad
fernandoc1 <fernandoc@epidataconsulting.com>
2020-12-09 05:17:36 -0300

    Updates

---
+++ precache-manifest.2c00986d0d6cc061cf022cbd9a8946f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c82afcf72fbf6b63af8ceb81b7e21f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7fecace927aab919cf96",
+    "url": "/portfolio/static/js/2.9247af00.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/portfolio/static/js/2.9247af00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c550870608c5e6ea3ec3",
+    "url": "/portfolio/static/js/main.4c6933d1.chunk.js"
+  },
+  {
+    "revision": "36b6a3ab7e9ecbaa474c",
+    "url": "/portfolio/static/js/runtime-main.1e03bee3.js"
+  }
+]);
