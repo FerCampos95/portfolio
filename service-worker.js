ead9b4e0d8e7a151957e626acacd949bd75a36c0
fernandoc1 <fernandoc@epidataconsulting.com>
2020-12-09 14:40:30 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2c00986d0d6cc061cf022cbd9a8946f6.js"
+  "/portfolio/precache-manifest.233781c80ac56f60f90260270dcfcdfb.js"
 );
 
 self.addEventListener('message', (event) => {
