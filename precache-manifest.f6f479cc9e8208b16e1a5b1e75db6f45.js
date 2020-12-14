7d67f1c237033bfc3c3ca15c0cb782312a524e93
FerCampos95 <FerCampos95@users.noreply.github.com>
2020-12-14 00:38:38 -0300

    Updates

---
+++ precache-manifest.f6f479cc9e8208b16e1a5b1e75db6f45.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b97f37a21b6381f84ac6049b90f812",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b9dae85282aceead4645",
+    "url": "/portfolio/static/js/2.fe46adcf.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/portfolio/static/js/2.fe46adcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df19a6cd6c544cde10df",
+    "url": "/portfolio/static/js/main.2d24b765.chunk.js"
+  },
+  {
+    "revision": "36b6a3ab7e9ecbaa474c",
+    "url": "/portfolio/static/js/runtime-main.1e03bee3.js"
+  },
+  {
+    "revision": "3fb4f41181adcd780c1d949a31183aae",
+    "url": "/portfolio/static/media/chat.3fb4f411.png"
+  },
+  {
+    "revision": "6b9251ba2cf0a40ac90bec424662ef26",
+    "url": "/portfolio/static/media/checkout.6b9251ba.png"
+  },
+  {
+    "revision": "73707ad9dbca412797df209af4acda4a",
+    "url": "/portfolio/static/media/fer-campos.73707ad9.png"
+  },
+  {
+    "revision": "81369c9a351211e070181eb15040205d",
+    "url": "/portfolio/static/media/fullstack.81369c9a.jpg"
+  },
+  {
+    "revision": "465058d5621c6d009fd7739b1cc92b53",
+    "url": "/portfolio/static/media/fullstackcielo.465058d5.png"
+  },
+  {
+    "revision": "2e4f085daf15382184a0facb4cf100fb",
+    "url": "/portfolio/static/media/notas.2e4f085d.png"
+  },
+  {
+    "revision": "440d8d2a114d13bc6b28932650912204",
+    "url": "/portfolio/static/media/tuttifrutti.440d8d2a.png"
+  }
+]);
